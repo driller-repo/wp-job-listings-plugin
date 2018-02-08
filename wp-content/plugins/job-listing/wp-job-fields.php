bdfa8987ab4551c5e351beb4d733b6ce3b5aaa14
driller <drillerone@protonmail.com>
2018-02-08 17:18:34 +0200

    added texteditor for wp-joblistings meta field

--- wp-content/plugins/job-listing/wp-job-fields.php
+++ wp-content/plugins/job-listing/wp-job-fields.php
@@ -7,3 +7,36 @@ function dwwp_add_custom_metabox() {
 }
 
 add_action('add_meta_boxes', 'dwwp_add_custom_metabox');
+
+function dwwp_meta_callback () {
+    ?>
+
+    <div>
+        <div class="meta-row">
+            <div class="meta-th">
+                <label for="job-id" class="dwwp-row-title">Job ID</label>
+            </div>
+            <div class="meta-td">
+                <input type="text" name="job-id" id="job-id" value="">
+            </div>
+        </div>
+    </div>
+    <div class="meta">
+        <div class="meta-th">
+            <span>
+                Principal Duties
+            </span>
+        </div>
+    </div>
+    <div class="meta-editor"></div>
+    <?php
+
+    $content = get_post_meta($post->ID, 'principle_duties', true);
+    $editor = 'principle_duties';
+    $settings = [
+        'textarea_rows' => 5,
+        'media_buttons'=> false
+    ];
+
+    wp_editor($content, $editor, $settings);
+}
--- wp-content/plugins/job-listing/wp-job-render-admin.php
+++ wp-content/plugins/job-listing/wp-job-render-admin.php
@@ -1,7 +1,1 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: developer
- * Date: 08.02.18
- * Time: 16:43
- */
