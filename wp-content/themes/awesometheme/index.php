57ccefe0acbb0a83837b29d03d51a28833ccc267
driller <drillerone@protonmail.com>
2018-02-08 12:47:25 +0200

    display blog posts on /blog page

--- wp-content/themes/awesometheme/index.php
+++ wp-content/themes/awesometheme/index.php
@@ -1,5 +1,24 @@
 <?php get_header() ?>
 
-    <h1>This is index page</h1>
+    <?php
+
+        if (have_posts()) {
+
+            while (have_posts()) {
+                the_post(); ?>
+
+                <h3><?php the_title(); ?></h3>
+
+                <small>Published on <?php the_time('F j, Y') ?>, in <?php the_category() ?></small>
+
+                <p><?php the_content(); ?></p>
+
+                <hr>
+
+            <?php }
+
+        }
+
+    ?>
 
 <?php get_footer() ?>
